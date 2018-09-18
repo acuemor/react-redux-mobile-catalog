0537e237f7b250f5853db5186dd8be88a7b34e6a
Abel Cuevas <abel.cuevas.moreno@gmail.com>
2018-09-18 09:33:45 +0200

    Applying styles

--- src/components/PhoneListComponent.js
+++ src/components/PhoneListComponent.js
@@ -2,22 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Phone from './Phone';
 
-const PhoneList = ({ phoneList = [{id: 'test', test: 'test2'}], onPhoneClick }) => (
-    <ul>
-        {phoneList.map(phone =>
-            <Phone
-                key={phone.id}
-                {...phone}
-                onClick={() => onPhoneClick(phone.id)}
-            />
-        )}
-    </ul>
+const PhoneList = ({ phoneList = [], onPhoneListClick }) => (
+    <div>
+        <ul>
+            {phoneList.map(phone =>
+                <Phone
+                    key={phone.key}
+                    {...phone}
+                    onClick={() => onPhoneListClick(phone.key)}
+                />
+            )}
+        </ul>
+    </div>
 )
 
 PhoneList.propTypes = {
-    onClick: PropTypes.string,
+    onPhoneListClick: PropTypes.func,
     inStock: PropTypes.bool,
     text: PropTypes.string
-  }
+}
 
 export default PhoneList
