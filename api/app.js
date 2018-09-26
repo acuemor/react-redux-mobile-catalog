8b82ed3038d510193a973ad2151ac89f0ec22b55
Abel Cuevas <abel.cuevas.moreno@gmail.com>
2018-09-26 10:25:55 +0200

    change images to static

--- api/app.js
+++ api/app.js
@@ -4,16 +4,20 @@ var routes = require("./routes/routes.js");
 
 var app = express();
 
-app.use((req, res, next) => { 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    return next(); 
+app.use("/images", express.static(__dirname + "/images"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  return next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
-var server = app.listen(3001, function () {
-    console.log("Server app running on port", server.address().port);
+var server = app.listen(3001, function() {
+  console.log("Server app running on port", server.address().port);
 });
